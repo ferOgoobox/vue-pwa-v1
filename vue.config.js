ac2fde7c577ae474f2ac76d0da3f265f6b0bfc6f
Laura Manzo <lauramanzo@Mac-mini-de-Laura.local>
2023-02-09 15:55:27 -0600

    workbx

--- vue.config.js
+++ vue.config.js
@@ -2,11 +2,11 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
   pwa: {
-    name: 'PWA Oventa',
-    workboxPluginMode: 'InjectManifest',
+    name: "PWA Oventa",
+    workboxPluginMode: "InjectManifest",
     workboxOptions: {
-      swSrc: 'src/sw.js',
+      swSrc: "src/sw.js",
       swDest: "service-worker.js",
-    }
-  }
+    },
+  },
 })
