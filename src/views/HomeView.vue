f152480e2994ca3759f0220931aaf91c5a672429
Laura Manzo <lauramanzo@Mac-mini-de-Laura.local>
2023-02-11 11:36:05 -0600

    pouchdb y api

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -4,58 +4,83 @@ import PouchDB from 'pouchdb';
 import { ref, onMounted } from 'vue'
 
 
-// const search = (event) => {
-//   const text = event.target.value;
-//     productsSearch.value = products.value;
-//     if(text && text.trim() != ''){
-//       productsSearch.value = products.value.filter((product)=>{
-//         return (product.title.toLowerCase().indexOf(text.toLowerCase()) > -1);
-//       })
-//     }else{
-//       productsSearch.value = []
-//     }
-// }
+
 
 const items = ref([]);
+const productsSearch = ref([])
+const online = ref(false)
 let db;
 
   onMounted(() => {
     db = new PouchDB('products');
-    loadFromDB();
   })
 
   const loadProducts = async() => {
-    const response = await axios.get('https://dummyjson.com/products');
-    const products =  response.data.products;
-    products.forEach(async product => {
-        await db.put({
-        _id: JSON.stringify(product.id),
-        title: product.title
-      }).then(response => {
-        console.log(response)
+    await axios.get('https://dummyjson.com/products')
+    .then(response => {
+      items.value = response.data.products
+      const products =  response.data.products;
+      products.forEach(async product => {
+          await db.put({
+          _id: JSON.stringify(product.id),
+          title: product.title
+        }).catch(error => {
+          console.log('Error al guardar en pouchDB',error)
+        })
+      });
+    })
+    .catch(async err => {
+      console.log(err)
+      const docs = await db.allDocs({ include_docs: true });
+      items.value = docs.rows.map(row => row.doc);
+      console.log('Vientos')
+    })
+  }
 
-      }).catch(error => {
-        console.log(error)
+  // const loadFromDB = async() => {
+  //   const docs = await db.allDocs({ include_docs: true });
+  //   items.value = docs.rows.map(row => row.doc);
+  // }
+
+  const search = (event) => {
+    console.log('entrasearch')
+    const text = event.target.value;
+    productsSearch.value = items.value;
+    if(text && text.trim() != ''){
+      productsSearch.value = items.value.filter((product)=>{
+        return (product.title.toLowerCase().indexOf(text.toLowerCase()) > -1);
       })
-
-    });
-    loadFromDB();
+    }else{
+      productsSearch.value = []
+    }
   }
 
-  const loadFromDB = async() => {
-    const docs = await db.allDocs({ include_docs: true });
-    items.value = docs.rows.map(row => row.doc);
+  // Detectar cambios de conexión
+  function isOnline() {
+    if ( navigator.onLine ) {
+       online.value = true
+    } else{
+       online.value = false
+    }
   }
+
+  window.addEventListener('online', isOnline );
+  window.addEventListener('offline', isOnline );
+
+  isOnline();
 </script>
 
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
+    <h4 v-if="online">Online</h4>
+    <h4 v-else>Offline</h4>
 
     <h1>Productos</h1>
+    <input type="text" @input="search($event)" placeholder="Ingresa un producto">
     <button @click="loadProducts">Cargar productos</button>
     <ul>
-      <li v-for="item in items" :key="item._id">{{ item.title }}</li>
+      <li v-for="item in productsSearch" :key="item._id">{{ item.title }}</li>
     </ul>
   </div>
 </template>
