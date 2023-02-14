2e311933bca818dcaf8535d8709b992d3c80de85
Laura Manzo <lauramanzo@Mac-mini-de-Laura.local>
2023-02-14 00:33:38 -0600

    sincronizando

--- src/views/OfflineView.vue
+++ src/views/OfflineView.vue
@@ -1,5 +1,35 @@
+<script setup>
+  import { useRouter } from 'vue-router';
+  import { ref } from 'vue'
+
+
+  const online = ref(false)
+  const router = useRouter()
+
+  const sincronizar = () =>{
+    console.log('sincronizando...')
+    router.push('/')
+  }
+
+  // Detectar cambios de conexión
+  function isOnline() {
+    if ( navigator.onLine ) {
+       online.value = true
+    } else{
+       online.value = false
+    }
+  }
+
+  window.addEventListener('online', isOnline );
+  window.addEventListener('offline', isOnline );
+
+</script>
 <template>
-  <div class="about">
+  <div>
+    <div v-if="online">
+      <h3> Se ha restablecido la conexión, sincroniza y regresa a la vista principal</h3>
+      <button @click="sincronizar()">Sincronizar</button>
+    </div>
     <h1>This is an offline page</h1>
   </div>
 </template>
